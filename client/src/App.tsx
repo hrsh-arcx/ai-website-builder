4a70453feb12471b3ae0ceee2b6a14c9e16b8d80
hrsh-arcx <harsh18goel@gmail.com>
2026-06-16 01:50:57 +0530

    chore: remove unused variables and clean up code for production

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import MyProjects from './pages/MyProjects'
